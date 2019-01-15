c2aef87634431d515c24fcfe691a84bf5a0a3fa7
Andrey-C-2018 <42644132+Andrey-C-2018@users.noreply.github.com>
2019-01-15 18:36:55 +0200

    CMultipleWidget

--- grid/MultipleCellWidget.cpp
+++ grid/MultipleCellWidget.cpp
@@ -1,5 +1,27 @@
 #include "MultipleCellWidget.h"
 
-CMultipleCellWidget::CMultipleCellWidget(){ }
+CMultipleCellWidget::CMultipleCellWidget() : is_created(false) { }
 
-CMultipleCellWidget::~CMultipleCellWidget(){ }
+void CMultipleCellWidget::SetCurrentField(const size_t field) {
+
+	if (IsWidgetPresent(field)) curr_widget = widgets[field];
+	else curr_widget = default_widget;
+}
+
+void CMultipleCellWidget::Show() {
+
+	curr_widget->Show();
+}
+
+void CMultipleCellWidget::Hide() {
+
+	curr_widget->Hide();
+}
+
+CMultipleCellWidget::~CMultipleCellWidget(){
+
+	if (is_created) return;
+
+	for (auto &widget : widgets)
+		delete widget.second;
+}
--- grid/MultipleCellWidget.h
+++ grid/MultipleCellWidget.h
@@ -2,12 +2,38 @@
 #include <map>
 #include "IGridCellWidget.h"
 
-class XWidget;
-
 class CMultipleCellWidget : public IGridCellWidget {
-	std::map<size_t, XWidget *> widgets;
+
+	IGridCellWidget *curr_widget, *default_widget;
+	bool is_created;
+	std::map<size_t, IGridCellWidget *> widgets;
 public:
 	CMultipleCellWidget();
+
+	void SetCurrentField(const size_t field);
+
+	void CreateCellWidget(XWindow *parent, const int flags, \
+							const Tchar *label, \
+							const int x, const int y, \
+							const int width, const int height) override;
+
+	void SetOnChangeHandler(XEventHandlerData on_change) override;
+	void SetOnLooseFocusHandler(XEventHandlerData on_loose_focus) override;
+	void SetOnKeyPressHandler(XEventHandlerData on_key_press) override;
+
+	void SetCurrentField(const size_t field) override;
+
+	void Show() override;
+	void Hide() override;
+	void MoveWindow(const int x, const int y, \
+					const int width, const int height) override;
+	void SetFocus() override;
+
+	ImmutableString<Tchar> GetLabel() override;
+	void SetLabel(ImmutableString<Tchar> label) override;
+
+	int GetId() const override;
+
 	virtual ~CMultipleCellWidget();
 };
 
