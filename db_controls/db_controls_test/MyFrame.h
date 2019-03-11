135f81caab03055bf2ae4a8ecfd17f764fa8cb09
Andrey-C-2018 <42644132+Andrey-C-2018@users.noreply.github.com>
2019-03-11 18:43:47 +0200

    ordersdb_frontend project added

--- db_controls/db_controls_test/MyFrame.h
+++ db_controls/db_controls_test/MyFrame.h
@@ -1,24 +1,12 @@
 ﻿#pragma once
 #include <xwindows/XFrame.h>
-#include <xwindows/XButton.h>
-#include <xwindows/XButton.h>
-#include <db_controls/DbGrid.h>
 
 class CMyFrame : public XFrame{
-	XButton *button_focus, *button_add, *button_remove, *button_refresh;
-	CDbGrid *grid;
 
 public:
 	CMyFrame(const Tchar *class_name,\
 			 const Tchar *label, const int X, const int Y,\
 			 const int width, const int height);
-
-	void OnSize(XSizeEvent *eve);
-
-	void OnButtonFocusClick(XCommandEvent *eve);
-	void OnButtonAddClick(XCommandEvent *eve);
-	void OnButtonRemoveClick(XCommandEvent *eve);
-	void OnButtonRefreshClick(XCommandEvent *eve);
 
 	virtual ~CMyFrame();
 };
---
+++ ordersdb_frontend/MyApp.cpp
@@ -0,0 +1,17 @@
+﻿#include "MyApp.h"
+
+IMPLEMENT_APP(CMyApp)
+
+void CMyApp::OnInit(){
+
+	frame = nullptr;
+	frame = new CMyFrame(_T("База даних доручень адвокатам"), \
+					_T("ORDERSDB_FRONTEND"), \
+				   20, 20,\
+				   800, 600);
+}
+
+CMyApp::~CMyApp(){
+
+	if(frame) delete frame;
+}
