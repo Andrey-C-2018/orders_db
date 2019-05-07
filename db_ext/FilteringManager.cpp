583b5825a85b6ea7f35b2797354030bcfa0c6398
Andrey-C-2018 <c_andrey@ukr.net>
2019-05-07 23:26:18 +0300

    FilteringManager ready for test

--- db_ext/FilteringManager.cpp
+++ db_ext/FilteringManager.cpp
@@ -1,73 +1,110 @@
+#include <assert.h>
+#include <db/IDbBindingTarget.h>
 #include "FilteringManager.h"
 
-CFilteringManager::CFilteringManager(std::shared_ptr<CDbTable> db_table_, \
-										ImmutableString<char> query_text) : \
-							db_table(db_table_), query_modifier(query_text) { }
-
-void CFilteringManager::setDefaultFilterExpr(ImmutableString<char> expression) {
-
-}
+CFilteringManager::CFilteringManager(ImmutableString<char> query_text_) : \
+									query_text(query_text_.str), \
+									filtering_changed(false) { }
 
 int CFilteringManager::addExpr(ImmutableString<char> expression, \
-				std::unique_ptr<IBinder> binder) {
+								std::unique_ptr<IBinder> binder) {
 
-	if(!disabled_exprs_count){
+	assert(!expression.isNull());
+	assert(binder);
+
+	FilteringItem item;
+	size_t size = filtering_items.size();
+	item.id = size ? filtering_items[size - 1].id + 1 : 0;
+	item.expression = expression.str;
+
+	item.pos_begin = 0;
+	item.pos_end = 0;
+	item.enabled = false;
+	item.switched = false;
+	item.first_param = -1;
+
+	item.binders.emplace_back(std::move(binder));
+	filtering_items.emplace_back(item);
 		
-		query_modifier.addExprToWhereStmt(expression);
-
-		FilteringItem item;
-		item.expression = expression.str;
-		item.index = query_modifier.getExprCount() - 1;
-		item.disabled = false;
-
-		filtering_items.push_back(item);
-		item.binders.emplace_back(binder);
-	}
-	else {
-		
-		query_modifier.setExpr(disabled_exprs_count - 1, expression);
-		--disabled_exprs_count;
-	}
-
-	db_table.setQuery(query_modifier.getQuery());
+	return item.id;
 }
 
 void CFilteringManager::addBinderToExpr(const int id_expr, std::unique_ptr<IBinder> binder) {
 
+	assert(binder);
+
+	auto p = findFilteringItem(id_expr);
+	assert(isFound(p, id_expr));
+
+	p->binders.emplace_back(std::move(binder));
 }
 
 void CFilteringManager::enableExpr(int id_expr) {
 
+	auto p = findFilteringItem(id_expr);
+	assert(isFound(p, id_expr));
+
+	bool prev_switched = p->switched;
+	if(!p->switched && switched_expr_count < MAX_SWITCHED_EXPR_COUNT) {
+		
+		p->expression.insert(0, "? OR (");
+		p->expression += ')';
+
+		p->switched = true;
+		++switched_expr_count;
+	}
+	
+	if (!isExprAddedToQuery(*p) && \
+		(prev_switched != p->switched || (!p->switched && !p->enabled))) {
+	
+		auto pos = addExprToWhereStmt(query_text, p->expression);
+		p->pos_begin = pos.first;
+		p->pos_end = pos.second;
+	}
+
+	p->enabled = true;
+	filtering_changed = true;
 }
 
 void CFilteringManager::disableExpr(int id_expr) {
 
+	auto p = findFilteringItem(id_expr);
+	assert(isFound(p, id_expr));
+
+	if (isExprAddedToQuery(*p) && !p->switched && p->enabled) {
+
+		query_text.erase(p->pos_begin, p->pos_end - p->pos_begin);
+		p->pos_begin = 0;
+		p->pos_end = 0;
+	}
+
+	p->enabled = false;
+	filtering_changed = true;
 }
 
-void CFilteringManager::resetFilterToDefault() {
+void CFilteringManager::apply(std::shared_ptr<IDbBindingTarget> parsed_query) {
 
+	if (!filtering_changed) return;
+
+	assert(parsed_query);
+
+	int params_counter = 0;
+	for(auto p = filtering_items.begin(); p != filtering_items.end(); ++p) {
+
+		p->first_param = params_counter;
+		if (p->switched) {
+			parsed_query->bindValue(p->first_param, p->enabled);
+			++params_counter;
+		}
+
+		if(p->enabled)
+			for(auto p_binder = p->binders.begin(); \
+				p_binder != p->binders.end(); ++p_binder, ++params_counter)
+					p_binder->get()->bind(parsed_query, params_counter);
+
+	}
+
+	filtering_changed = false;
 }
 
-void CFilteringManager::setDefaultOrderingValue(ImmutableString<char> expression) {
-
-}
-
-void CFilteringManager::setOrderingField(const bool asc) {
-
-}
-
-void CFilteringManager::resetOrderingToDefault() {
-
-}
-
-void CFilteringManager::resetAllToDefault() {
-
-}
-
-void CFilteringManager::apply() {
-
-}
-
-CFilteringManager::~CFilteringManager() {
-
-}
+CFilteringManager::~CFilteringManager() { }
--- db_ext/FilteringManager.h
+++ db_ext/FilteringManager.h
@@ -1,70 +1,119 @@
 #include <vector>
 #include <memory>
+#include <algorithm>
 #include <basic/ImmutableString.h>
 #include <db_ext/IBinder.h>
 
-class CQueryModifier final {
-	ImmutableString<char> query;
-	size_t expr_count;
-	bool no_where_stmt, ordering_enabled;
-public:
-	CQueryModifier(ImmutableString<char> query);
-
-	void getQuery() const;
-	inline size_t getExprCount() const;
-	void addExprToWhereStmt(ImmutableString<char> expr);
-	void setExpr(const size_t expr_index, ImmutableString<char> expr);
-	void removeExprFromWhereStmt(const size_t expr_index);
-	void setWhereStmt(ImmutableString<char> where_stmt);
-	
-	void setOrdering(const size_t field);
-	void setOrdering(ImmutableString<char> expr);
-	void removeOrdering();
-};
-
-class CDbTable;
-
 class CFilteringManager {
 	enum {
-		DISABLED_EXPR_LIMIT = 5
+		MAX_SWITCHED_EXPR_COUNT = 5
 	};
 
-	std::shared_ptr<CDbTable> db_table;
-	CQueryModifier query_modifier; 
+	std::string query_text;
+	bool filtering_changed;
 
 	struct FilteringItem {
+		int id;
 		std::string expression;
-		size_t index;
-		bool disabled;
+		size_t pos_begin, pos_end;
+		bool enabled;
+		bool switched;
 		std::vector<std::unique_ptr<IBinder> > binders;
+		int first_param;
+	};
+
+	class CSearchPredicate {
+		const std::vector<FilteringItem> &filtering_items;
+	public:
+		CSearchPredicate(const std::vector<FilteringItem> &filtering_items_) : \
+							filtering_items(filtering_items_) { }
+		inline bool operator()(const int id_expr, const FilteringItem &item) {
+
+			return id_expr < item.id;
+		}
+
+		inline bool operator()(const FilteringItem &item, const int id_expr) {
+
+			return item.id < id_expr;
+		}
 	};
 
 	std::vector<FilteringItem> filtering_items;
-	size_t disabled_exprs[DISABLED_EXPR_LIMIT];
-	size_t disabled_exprs_count;
+	size_t switched_expr_count;
 
-	size_t field_to_order;
+	auto findFilteringItem(const int id_expr) const;
+	auto findFilteringItem(const int id_expr);
+	inline bool isFound(std::vector<FilteringItem>::const_iterator, \
+						const int id_expr) const;
+	inline bool isFound(std::vector<FilteringItem>::iterator, \
+						const int id_expr) const;
+	inline static std::pair<size_t, size_t> \
+			addExprToWhereStmt(std::string &text, \
+								const std::string &expression);
+	inline bool isExprAddedToQuery(const FilteringItem &item) const;
 
-	std::string def_filter_expr;
-	std::string def_ordering;
 public:
-	CFilteringManager(std::shared_ptr<CDbTable> db_table_, \
-				ImmutableString<char> query_text);
+	CFilteringManager(ImmutableString<char> query_text);
 
-	void setDefaultFilterExpr(ImmutableString<char> expression);
 	int addExpr(ImmutableString<char> expression, std::unique_ptr<IBinder> binder);
 	void addBinderToExpr(const int id_expr, std::unique_ptr<IBinder> binder);
 
 	void enableExpr(int id_expr);
+	void enableAll();
 	void disableExpr(int id_expr);
-	void resetFilterToDefault();
-
-	void setDefaultOrderingValue(ImmutableString<char> expression);
-	void setOrderingField(const bool asc);
-	void resetOrderingToDefault();
-
-	void resetAllToDefault();
-	void apply();
+	void disableAll();
+	
+	inline ImmutableString<char> getModifiedQuery() const;
+	void apply(std::shared_ptr<IDbBindingTarget> parsed_query);
 
 	virtual ~CFilteringManager();
 };
+
+//*****************************************************
+
+auto CFilteringManager::findFilteringItem(const int id_expr) const {
+
+	return std::lower_bound(filtering_items.cbegin(), filtering_items.cend(), \
+							id_expr, CSearchPredicate(filtering_items));
+}
+
+auto CFilteringManager::findFilteringItem(const int id_expr) {
+
+	return std::lower_bound(filtering_items.begin(), filtering_items.end(), \
+							id_expr, CSearchPredicate(filtering_items));
+}
+
+bool CFilteringManager::isFound(std::vector<FilteringItem>::const_iterator p, \
+								const int id_expr) const {
+
+	return p != filtering_items.cend() && p->id == id_expr;
+}
+
+bool CFilteringManager::isFound(std::vector<FilteringItem>::iterator p, \
+								const int id_expr) const {
+
+	return p != filtering_items.end() && p->id == id_expr;
+}
+
+std::pair<size_t, size_t> \
+		CFilteringManager::addExprToWhereStmt(std::string &text, \
+												const std::string &expression) {
+	std::pair<size_t, size_t> pos;
+	pos.first = text.size();
+
+	text += ", ";
+	text += expression;
+	pos.second = text.size();
+
+	return pos;
+}
+
+bool CFilteringManager::isExprAddedToQuery(const FilteringItem &item) const {
+
+	return item.pos_end && item.pos_begin != item.pos_end;
+}
+
+ImmutableString<char> CFilteringManager::getModifiedQuery() const {
+
+	return ImmutableString<char>(query_text.c_str(), query_text.size());
+}
